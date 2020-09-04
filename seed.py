67bb2a3cc5087cb56ec32dacfecf5c2b9c34c909
unknown <jreidmke@gmail.com>
2020-09-04 11:22:52 -0500

    init commit

---
+++ seed.py
@@ -0,0 +1,16 @@
+from app import app
+from models import db, User
+
+
+db.drop_all()
+db.create_all()
+
+user1 = User(username='James', password='Pizza', email='jreidmke@gmail', first_name='James', last_name='Reid')
+user2 = User(username='Maria', password='Pizza', email='mariareidmke@gmail', first_name='Maria', last_name='Reid')
+user3 = User(username='Oliver', password='Pizza', email='ollieg@gmail', first_name='Oliver', last_name='Germain')
+
+db.session.add(user1)
+db.session.add(user2)
+db.session.add(user3)
+
+db.session.commit()
