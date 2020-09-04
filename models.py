597c270ad38f05e044bb001d9a9fe63e6cd9f701
unknown <jreidmke@gmail.com>
2020-09-04 11:22:03 -0500

    created User model

---
+++ models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    username = db.Column(db.String, unique = True, primary_key=True)
+    password = db.Column(db.String, nullable = False)
+    email = db.Column(db.String(50), nullable = False)
+    first_name = db.Column(db.String(30), nullable = False)
+    last_name = db.Column(db.String(30), nullable = False)
