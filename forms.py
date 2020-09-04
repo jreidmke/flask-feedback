21028168fae337b7d56250e93f02fb587782e839
unknown <jreidmke@gmail.com>
2020-09-04 11:49:38 -0500

    added user register form

--- forms.py
+++ forms.py
@@ -1,3 +1,11 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, PasswordField
-# from wtforms.validators import InputRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    # retype_password = PasswordField('Retype Password', validators=[InputRequired()])
+    email = StringField('Email Address', validators=[InputRequired()])
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
