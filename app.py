c9d4a0f4cb3ec421928f4803e1334e6b10a3b82c
unknown <jreidmke@gmail.com>
2020-09-04 11:49:51 -0500

    added register route

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask, request, jsonify, render_template
-
+from flask import Flask, request, session, render_template, redirect, flash, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
+from forms import RegisterForm
+
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres4@localhost/feedback'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = "oh-so-secret"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["SECRET_KEY"] = "abc123"
+
+connect_db(app)
+db.create_all()
+
+toolbar = DebugToolbarExtension(app)
+
+@app.route('/')
+def redirect_register():
+    return redirect('/register')
+
+@app.route('/register')
+def show_register_form():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
